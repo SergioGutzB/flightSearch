7114ce51d097e0891a94f1a9fbea2114ccdd2fb2
fab48 <fab7696550@gmail.com>
2016-11-07 17:49:47 -0500

    add rest for search winther

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
-var express = require('express')
-var app = express()
+var express = require('express'),
+    app = express();
+
+var clima = { 
+  'bogota': {
+    'temperatura': 25,
+    'humedad': 12
+  },
+  'medellin': {
+    'temperatura': 10,
+    'humedad': 5
+  },
+  'ciudad de mexico': {
+    'temperatura': 30,
+    'humedad': 25
+  },
+  'barranquilla': {
+    'temperatura': 25,
+    'humedad': 13
+  },
+}
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hola accede a /clima para saber el clima de un lugar')
 })
+
+app.get('/clima', function(req, res){
+  res.send('Debes enviar una peticion POST con el parametro ciudad con el nombre de la ciudad')
+});
+
+app.post('/clima', function(req, res){
+  var data = clima[req['query']['ciudad']];
+  res.end(JSON.stringify(data));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
