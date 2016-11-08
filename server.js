8e01a6420064e08aa7f7bbb16c9576064e7b02e6
fab48 <fab7696550@gmail.com>
2016-11-07 21:01:52 -0500

    add function random

--- server.js
+++ server.js
@@ -1,38 +1,48 @@
 var express = require('express'),
     app = express();
 
-var clima = { 
-  'bogota': {
-    'temperatura': 25,
-    'humedad': 12
-  },
-  'medellin': {
-    'temperatura': 10,
-    'humedad': 5
-  },
-  'ciudad de mexico': {
-    'temperatura': 30,
-    'humedad': 25
-  },
-  'barranquilla': {
-    'temperatura': 25,
-    'humedad': 13
-  },
+function pronostico(fecha){
+  return Math.floor(Math.random()*(100 - 0)) ;
 }
 
+var clima = function(lugar, fecha){
+  var base = { 
+    'bogota': {
+      'temperatura': pronostico(fecha),
+      'humedad': pronostico(fecha),
+    },
+    'medellin': {
+      'temperatura': pronostico(fecha),
+      'humedad': pronostico(fecha),
+    },
+    'ciudad de mexico': {
+      'temperatura': pronostico(fecha),
+      'humedad': pronostico(fecha),
+    },
+    'barranquilla': {
+      'temperatura': pronostico(fecha),
+      'humedad': pronostico(fecha),
+    },
+  };
+  return base[lugar];
+
+};
+//RAIZ DE VISITA DEL SITIO
 app.get('/', function (req, res) {
   res.send('Hola accede a /clima para saber el clima de un lugar')
 })
 
-app.get('/clima', function(req, res){
+//PETICION PARA VER EL CLIMA ACTUAL DE UN SITIO
+app.get('/clima_actual', function(req, res){
   res.send('Debes enviar una peticion POST con el parametro ciudad con el nombre de la ciudad')
 });
 
-app.post('/clima', function(req, res){
-  var data = clima[req['query']['ciudad']];
+app.post('/clima_actual', function(req, res){
+  var data = clima(req['query']['ciudad'], req['query']['fecha']);
   res.end(JSON.stringify(data));
 });
 
+//SE ESCUCHA PARA SERVIR
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
